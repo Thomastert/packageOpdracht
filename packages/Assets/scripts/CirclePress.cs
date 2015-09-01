3a082691ef7b72224956320adf473b5fe5d80edf
Thomastert <thomas@thoemda.com>
2015-09-01 12:33:02 +0200

    hi
    hi

---
+++ packages/Assets/scripts/CirclePress.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclePress : MonoBehaviour {
+	
+	
+	void OnMouseDown()
+	{
+		Application.LoadLevel("menu");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ packages/Assets/scripts/changeScene1.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Timers;
+
+public class changeScene1 : MonoBehaviour {
+
+
+	public float timer = 54;
+
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Application.isEditor) {
+		
+			Application.LoadLevel ("menu");
+		} else {
+			if (timer > 0) {
+				timer -= Time.deltaTime;
+			}
+
+			if (timer <= 0) {
+				Application.LoadLevel ("menu");
+			}
+		}
+	}
+}
